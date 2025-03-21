0f35266195afe6b00d2002ef98450ee93145334f
Mykyta S. Oleshchenko <130276994+NureOleshchenkoMykyta@users.noreply.github.com>
2025-03-21 16:06:37 +0200

    Update pzpi-22-5-oleshchenko-mykyta-design-pattern.ts

--- Pract1/pzpi-22-5-oleshchenko-mykyta-design-pattern/pzpi-22-5-oleshchenko-mykyta-design-pattern.ts
+++ Pract1/pzpi-22-5-oleshchenko-mykyta-design-pattern/pzpi-22-5-oleshchenko-mykyta-design-pattern.ts
@@ -1,1 +1,36 @@
+interface State {
+    handle(): void;
+}
 
+class Context {
+    state: State;
+  
+    constructor(initialState: State) {
+    this.state = initialState;
+    }
+  
+    setState(state: State) {
+    this.state = state;
+    }
+  
+    handle() {
+    this.state.handle();
+    }
+}
+
+class ConcreteStateA implements State {
+    handle() {
+    console.log("ConcreteStateA");
+    }
+}
+
+class ConcreteStateB implements State {
+    handle() {
+    console.log("ConcreteStateB");
+    }
+}
+
+const context = new Context(new ConcreteStateA());
+context.handle();
+context.setState(new ConcreteStateB());
+context.handle();
